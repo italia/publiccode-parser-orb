68d9a451d3439d0317cc1a572254529c3bd2f082
romain325 <romainolivier42@gmail.com>
2020-10-07 15:30:19 +0200

    Test for docker2

--- src/scripts/pcvalidate.sh
+++ src/scripts/pcvalidate.sh
@@ -1,10 +1,15 @@
 Validation() {
+    echo "Start Validation Test"
+    echo $(docker -v)
+
     FILEPATH=~/publiccodes
     [ -n "${PARAM_FPATH}" ] && echo "${PARAM_FPATH}" | grep -Eq '^\/.*' && FILEPATH="${PARAM_FPATH}" || FILEPATH=$FILEPATH"/${PARAM_FPATH}"
     [ -z "${P_REMOTE_BASE_URL}" ] && REMOTEPATH="" || REMOTEPATH="-remote-base-url ${P_REMOTE_BASE_URL}"
     [ -n "${P_DISABLE_STRICT}" ] && STRICT="-no-strict" || STRICT=""
     [ -n "${P_DISABLE_NETWORK}" ] && NETWORK="-no-network" || NETWORK=""
+    echo "Command:"
     echo "docker run -v" "$FILEPATH" "italia/publiccode-parser-go" "${P_FILENAME}" "$REMOTEPATH" "$STRICT" "$NETWORK"
+    docker run hello-world
     docker run -v "$FILEPATH" italia/publiccode-parser-go "${P_FILENAME}" "$REMOTEPATH" "$STRICT" "$NETWORK"
 }
 
