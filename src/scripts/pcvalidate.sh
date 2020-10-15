4430fc8ff0bb1c4aef32e6f208984e58a500b4bc
romain325 <romainolivier42@gmail.com>
2020-10-15 08:35:01 +0200

    correct path param

--- src/scripts/pcvalidate.sh
+++ src/scripts/pcvalidate.sh
@@ -4,14 +4,14 @@ Validation() {
 
     FILEPATH="$(pwd)/"
 
-    [ -n "${PARAM_FPATH}" ] && echo "${PARAM_FPATH}" | grep -Eq '^\/.*' && FILEPATH="${PARAM_FPATH}/" || FILEPATH=$FILEPATH"${PARAM_FPATH}/"
+    [ -n "${PARAM_FPATH}" ] && echo "${PARAM_FPATH}" | grep -Eq '^\/.*' && FILEPATH="${PARAM_FPATH}/" || FILEPATH="$FILEPATH/${PARAM_FPATH}/"
     [ -z "${P_REMOTE_BASE_URL}" ] && REMOTEPATH="" || REMOTEPATH=" -remote-base-url ${P_REMOTE_BASE_URL} "
     [ "${P_DISABLE_STRICT}" = "true" ] && STRICT=" -no-strict " || STRICT=""
     [ "${P_DISABLE_NETWORK}" = "true" ] && NETWORK=" -no-network " || NETWORK=""
 
-    echo "docker run -i italia/publiccode-parser-go /dev/stdin $REMOTEPATH$STRICT$NETWORK < $FILEPATH${P_FILENAME}" 
+    echo "docker run -i italia/publiccode-parser-go /dev/stdin $REMOTEPATH$STRICT$NETWORK --verbose < $FILEPATH${P_FILENAME}" 
 
-    docker run -i italia/publiccode-parser-go /dev/stdin "$REMOTEPATH$STRICT$NETWORK" < "$FILEPATH${P_FILENAME}"
+    docker run -i italia/publiccode-parser-go /dev/stdin "$REMOTEPATH$STRICT$NETWORK" --verbose < "$FILEPATH${P_FILENAME}"
 }
 
 ORB_TEST_ENV="bats-core"
